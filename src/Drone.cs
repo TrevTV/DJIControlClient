5e35e68123baf712d6f0b9d77ae8e97c8b701c5c
Trevor <25110388+TrevTV@users.noreply.github.com>
2023-09-23 16:11:02 -0400

    Base code

---
+++ src/Drone.cs
@@ -0,0 +1,24 @@
+﻿namespace DJIControlClient
+{
+    public class Drone
+    {
+        private HttpClient _httpClient;
+
+        public Drone(string ipPort)
+        {
+            if (!ipPort.EndsWith('/'))
+                ipPort += '/';
+
+            _httpClient = new()
+            {
+                BaseAddress = new Uri(ipPort)
+            };
+        }
+
+        public async Task<bool> IsConnected()
+        {
+            string? connected = await _httpClient.GetStringAsync("");
+            return connected != null;
+        }
+    }
+}
